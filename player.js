5ed57964dec9e18193bbfeb1fa4ce7e69d6d19f5
xepozone <lilis.tsu@gmail.com>
2019-02-03 13:23:58 +0700

    Update player.js

--- player.js
+++ player.js
@@ -1,8 +1,8 @@
-loadPlayer = function(this) {
+function loadPlayer(me) {
     //e.preventDefault();
-    var download_link = $(this).data('baseid');
+    var download_link = $(me).data('baseid');
     $('.tbl_active_download').attr('href', baseURL + 'file?yb_id=' + download_link);
-    var index = $(this).data('id');
+    var index = $(me).data('id');
     var url_yb = 'https://www.youtube.com/watch?v=' + playlist[index];
     count = Object.keys(playlist).length - 1;
     var audio = document.getElementById('audio-30133-2');
@@ -10,12 +10,12 @@ loadPlayer = function(this) {
     if (source.src == url_yb) {
         if (audio.paused) {
             audio.play();
-            $(this).html(pause_icon);
-            $(this).closest('.cover-yb').css('background-color', 'rgba(0, 0, 0, 0.09)')
+            $(me).html(pause_icon);
+            $(me).closest('.cover-yb').css('background-color', 'rgba(0, 0, 0, 0.09)')
         } else {
             audio.pause();
-            $(this).closest('.cover-yb').css('background-color', '#ffffff');
-            $(this).html(yb_icon)
+            $(me).closest('.cover-yb').css('background-color', '#ffffff');
+            $(me).html(yb_icon)
         }
     } else {
         source.src = url_yb;
@@ -24,8 +24,8 @@ loadPlayer = function(this) {
         audio.play();
         $('.play-yb').html(yb_icon);
         $('.cover-yb').css('background-color', '#ffffff');
-        $(this).closest('.cover-yb').css('background-color', 'rgba(0, 0, 0, 0.09)');
-        $(this).html(pause_icon)
+        $(me).closest('.cover-yb').css('background-color', 'rgba(0, 0, 0, 0.09)');
+        $(me).html(pause_icon)
     }
     audio.addEventListener('ended', function(e) {
         index++;
