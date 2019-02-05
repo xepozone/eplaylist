eb61c5003efd3134077b935dd5537d45c0462d42
xepozone <lilis.tsu@gmail.com>
2019-02-05 11:08:51 +0700

    Delete p.js

--- p.js
+++
@@ -1,8 +0,0 @@
-$(document).on('click', '.mejs__pause', function() {
-    $('.play-yb').html(yb_icon);
-    $('.cover-yb').css('background-color', '#ffffff')
-});
-$(document).on('click', '.play-yb', function(e) {
-	e.preventDefault();
-	loadPlayer(this);
-})
